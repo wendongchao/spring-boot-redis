a1a7e8098d4bad8d15df86d6b5c77e076e0dc1be
wendongchao <dongchao1860369@sina.com>
2023-03-13 14:41:57 +0800

    美化swagger

--- src/main/java/com/example/redis/config/Swagger2Config.java
+++ src/main/java/com/example/redis/config/Swagger2Config.java
@@ -1,5 +1,6 @@
 package com.example.redis.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@EnableKnife4j
 public class Swagger2Config {
     @Bean
     public Docket createRestApi(){
